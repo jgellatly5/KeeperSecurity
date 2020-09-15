d19a47b0541653083074fbb4731aa8b36987751e
jgellatly5 <jgellatly5@gmail.com>
2020-09-15 06:50:49 -0700

    Adding email validation for the email input when the login button is pressed.

--- app/src/main/java/com/jordangellatly/loginkeeper/MainActivity.kt
+++ app/src/main/java/com/jordangellatly/loginkeeper/MainActivity.kt
@@ -2,9 +2,12 @@ package com.jordangellatly.loginkeeper
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
 import android.text.Spannable
 import android.text.SpannableString
+import android.text.TextWatcher
 import android.text.style.ForegroundColorSpan
+import android.widget.Toast
 import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -22,5 +25,19 @@ class MainActivity : AppCompatActivity() {
             Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
         )
         tv_create_account.text = spannable
+
+        btn_login.setOnClickListener {
+            checkIfEmailIsValid()
+        }
+    }
+
+    private fun checkIfEmailIsValid() {
+        val email: String = et_email.text.toString().trim()
+        val emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+"
+        if (email.matches(emailPattern.toRegex()) && email.isNotEmpty()) {
+            Toast.makeText(applicationContext, "Valid email address", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(applicationContext, "Please try again", Toast.LENGTH_SHORT).show()
+        }
     }
 }
